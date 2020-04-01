0bb090b14e61f0b7e596ff49a4ed5e555538280a
dispellizer <dispellizer@naver.com>
2020-04-01 20:34:39 +0900

    Updates

---
+++ precache-manifest.852feee123d81b0362a1e274113c6a08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91ac3aceb77a618e473862ef8692dcc1",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "daf41441744cbbda6d55",
+    "url": "/movie_app/static/css/main.10b0ebdd.chunk.css"
+  },
+  {
+    "revision": "1d2d5261e5e33a966d0f",
+    "url": "/movie_app/static/js/2.111c2b99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.111c2b99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daf41441744cbbda6d55",
+    "url": "/movie_app/static/js/main.db8d3857.chunk.js"
+  },
+  {
+    "revision": "c4e503dc5fc5f4a3fb01",
+    "url": "/movie_app/static/js/runtime-main.202e151f.js"
+  }
+]);
