c213821da7382db3dea36a98382cce0f45611b27
dispellizer <dispellizer@naver.com>
2020-04-01 17:14:34 +0900

    4.3 Adding Genres

--- src/Movie.js
+++ src/Movie.js
@@ -2,13 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-function Movie({ year, title, summary, poster }) {
+function Movie({ year, title, summary, poster, genres }) {
   return (
     <div className="movie">
       <img src={poster} alt={title} title={title}></img>
       <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
+        <ul className="movie__genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres__genre">
+              {genre}
+            </li>
+          ))}
+        </ul>
         <p className="movie__summary">{summary}</p>
       </div>
     </div>
@@ -20,7 +27,9 @@ Movie.propTypes = {
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
-  poster: PropTypes.string.isRequired
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired
+  //  arrayOf는 PropTypes에서 제공하는 함수, 해상 PropTypes로 구성된 배열
 };
 
 export default Movie;
